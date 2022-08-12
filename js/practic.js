a19b3fe543fdfba6bc195e1ef115bc7b4b1b4f29
Yevhenii Pavlushchenko <paw93@ukr.net>
2022-08-12 17:39:50 +0300

    #1

---
+++ js/practic.js
@@ -0,0 +1,19 @@
+const options = 1;
+let = " ";
+
+switch (Option) {
+    case 1:
+        message = "Вы можете забрать завтра в офисе";
+        break;
+        
+     case 2:
+        message = "Курьер доставит заказ с 09:00 до 18:00";
+        break;
+     case 3:
+        message = "Посылка будет отправлена сегодня";
+        break;
+    default:
+        message = "Менеджер с вами свяжется"
+}
+
+console.log(message);
